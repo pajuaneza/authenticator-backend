56c9fe5abd1f7533c3c31bd7cfebb8e74ea00f9f
Patrick Juaneza <pajuaneza@gmail.com>
2023-11-07 13:58:45 +0800

    Reformat code

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,24 +1,25 @@
 package controllers
 
+import java.util.UUID
 import javax.inject._
-import play.api._
-import play.api.mvc._
-import models.repo.EmpRepo
-import scala.concurrent._
-import play.api.libs.json._
 import models.domain.Emp
+import models.repo.EmpRepo
+import play.api._
 import play.api.data.Form
 import play.api.data.Forms._
-import java.util.UUID
+import play.api.libs.json._
+import play.api.mvc._
+import scala.concurrent._
 import security.Authenticator
 import security.UserRequest
 
 @Singleton
-class HomeController @Inject()(
+class HomeController @Inject() (
   authenticator: Authenticator,
   val empRepo: EmpRepo,
-  val cc: ControllerComponents
-)(implicit val ec: ExecutionContext) extends AbstractController(cc) {
+  val cc: ControllerComponents,
+)(implicit val ec: ExecutionContext)
+    extends AbstractController(cc) {
 
   val empForm = Form(
     mapping(
@@ -39,39 +40,51 @@ class HomeController @Inject()(
     )
   )
 
-  def index() = Action.async { implicit request: Request[AnyContent] =>
-    empRepo.createEmployeeTable().map { _ => Ok("Employee table created!")}
+  def index() = Action.async { implicit request =>
+    empRepo.createEmployeeTable().map { _ =>
+      Ok("Employee table created!")
+    }
   }
 
-  def getAllEmployees() = Action.async { implicit request: Request[AnyContent] =>
-    empRepo.getAllEmployees().map { employees => Ok(Json.toJson(employees))}
+  def getAllEmployees() = Action.async { implicit request =>
+    empRepo.getAllEmployees().map { employees =>
+      Ok(Json.toJson(employees))
+    }
   }
 
-  def addEmployee() = Action.async { implicit request: Request[AnyContent] =>
-    empForm.bindFromRequest().fold(
-      errors => {
-        Future.successful(BadRequest)
-      },
-      employee => {
-        empRepo.addEmployee(employee.copy(id = UUID.randomUUID())).map { _ => Ok("Employee added!")}
-      }
-    )
-  }
-
-  def login() = authenticator.async { implicit request: Request[AnyContent] =>
-    loginForm.bindFromRequest().fold(
-      error => {
-        Future.successful(Unauthorized)
-      },
-      credentials => {
-        empRepo.findEmployeeByUsernameAndPassword(credentials._1, credentials._2).map { employee =>
-          employee match {
-            case Some(em) =>
-              Ok(Json.toJson(em)).withSession("username" -> em.username)
-            case None => Unauthorized
+  def addEmployee() = Action.async { implicit request =>
+    empForm
+      .bindFromRequest()
+      .fold(
+        errors => {
+          Future.successful(BadRequest)
+        },
+        employee => {
+          empRepo.addEmployee(employee.copy(id = UUID.randomUUID())).map { _ =>
+            Ok("Employee added!")
           }
-        }
-      }
-    )
+        },
+      )
+  }
+
+  def login() = authenticator.async { implicit request =>
+    loginForm
+      .bindFromRequest()
+      .fold(
+        error => {
+          Future.successful(Unauthorized)
+        },
+        credentials => {
+          empRepo
+            .findEmployeeByUsernameAndPassword(credentials._1, credentials._2)
+            .map { employee =>
+              employee match {
+                case Some(em) =>
+                  Ok(Json.toJson(em)).withSession("username" -> em.username)
+                case None => Unauthorized
+              }
+            }
+        },
+      )
   }
 }
--- app/models/domain/Emp.scala
+++ app/models/domain/Emp.scala
@@ -3,19 +3,44 @@ package models.domain
 import java.util.UUID
 import play.api.libs.json._
 
-case class Emp (
+case class Emp(
   id: UUID,
   firstName: String,
   middleName: Option[String],
   lastName: String,
   username: String,
   password: String,
-  role: String
+  role: String,
 )
 
 object Emp {
-  val tupled = (apply: (UUID, String, Option[String], String, String, String, String) => Emp).tupled
-  def apply (firstName: String, middleName: Option[String], lastName: String,username: String, password: String, role: String): Emp = apply(UUID.randomUUID(), firstName, middleName, lastName, username, password, role)
+  val tupled = (apply: (
+    UUID,
+    String,
+    Option[String],
+    String,
+    String,
+    String,
+    String,
+  ) => Emp).tupled
+
+  def apply(
+    firstName: String,
+    middleName: Option[String],
+    lastName: String,
+    username: String,
+    password: String,
+    role: String,
+  ): Emp = apply(
+    UUID.randomUUID(),
+    firstName,
+    middleName,
+    lastName,
+    username,
+    password,
+    role,
+  )
+
   implicit val writes: Writes[Emp] = (emp: Emp) => {
     Json.obj(
       "id" -> emp.id.toString,
@@ -23,7 +48,7 @@ object Emp {
       "middleName" -> emp.middleName,
       "lastName" -> emp.lastName,
       "username" -> emp.username,
-      "role" -> emp.role
+      "role" -> emp.role,
     )
   }
 }
